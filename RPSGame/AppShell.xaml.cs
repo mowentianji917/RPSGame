dd8c490621dbf3bf9638627c596768ed6051424d
Max Z <liufangxingoudi@gmail.com>
2023-02-02 14:25:18 -0500

    Add project files.

---
+++ RPSGame/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RPSGame;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ RPSGame/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RPSGame;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
